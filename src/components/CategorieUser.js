b3350d4ee725c504521239018f87531c4fcc47b3
Taliayaya <ilanvinord@gmail.com>
2021-11-13 13:07:58 +0100

    Modification du système de Offline

--- src/components/CategorieUser.js
+++ src/components/CategorieUser.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 
-
-export default function CategorieUser({UserList}) {
-    const sortedUserList = UserList.sort((a,b) => a.role>b.role ? 1 : -1);
-    let categorie = '';
-    return (
-        <div className="user__list">
-            
-            {sortedUserList.map(({id, user, online, role}) => 
-            
-            (categorie!==role ?
-            (categorie=role,
+export default function CategorieUser({ UserList }) {
+  const sortedUserList = UserList.sort((a, b) =>
+    a.role > b.role ? 1 : -1
+  ).sort((a, b) => (a.online === b.online ? 0 : a.online ? -1 : 1));
+  let categorie = "";
+  return (
+    <div className="user__list">
+      {sortedUserList.map(({ id, user, online, role }) =>
+        categorie !== role && categorie !== "Offline" ? (
+          ((categorie = !online ? "Offline" : role),
+          (
             <>
-            <h4 key={role} className="categorie">{role}</h4>
-            <p key={id} className={online? "online":"offline"}>{user}</p></>)
-            : <p key={id} className={online? "online":"offline"}>{user}</p>))
-            }
-            
-        </div>
-    )
+              <h4 key={categorie} className="categorie">
+                {categorie}
+              </h4>
+              <p key={id} className={online ? "online" : "offline"}>
+                {user}
+              </p>
+            </>
+          ))
+        ) : (
+          <p key={id} className={online ? "online" : "offline"}>
+            {user}
+          </p>
+        )
+      )}
+    </div>
+  );
 }
