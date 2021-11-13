ca63a0e9d1ccfd791cfad312d5040b0ff52571ab
Taliayaya <ilanvinord@gmail.com>
2021-11-13 13:08:46 +0100

    Utilisation de Prettier. Aucune grosse modif

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import '../styles/App.css';
-import ChannelList from './ChannelList';
-import Chat from './Chat';
-import Footer from './Footer';
-import Header from './Header';
-import UsersList from './UsersList';
-
+import React from "react";
+import "../styles/App.css";
+import ChannelList from "./ChannelList";
+import Chat from "./Chat";
+import Footer from "./Footer";
+import Header from "./Header";
+import UsersList from "./UsersList";
 
 function App() {
   return (
@@ -13,13 +12,12 @@ function App() {
       {/* HEADER */}
       <Header />
       <div className="structure">
-    
         {/* CHANNEL LIST*/}
         <ChannelList />
-    
+
         {/* CHAT */}
         <Chat />
-    
+
         {/* USERS LIST */}
         <UsersList />
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import '../styles/Header.css';
-import { Avatar } from '@material-ui/core';
+import React, { Component } from "react";
+import "../styles/Header.css";
+import { Avatar } from "@material-ui/core";
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-
-                <div className="header__components">
-
-                    <div className="header__title">
-                        <h1>Pando</h1>           
-                    </div>
-                    <div className="header__avatar">
-                        <Avatar className="user__avatar" />
-                    </div>
-
-
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <div className="header__components">
+          <div className="header__title">
+            <h1>Pando</h1>
+          </div>
+          <div className="header__avatar">
+            <Avatar className="user__avatar" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
