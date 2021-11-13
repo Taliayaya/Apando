73e034a4a58cc66245be80925299301e6a95e5ef
Taliayaya <ilanvinord@gmail.com>
2021-11-13 13:06:18 +0100

    Précisions sur les rôles et les catégories : Offline

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import SettingsIcon from '@material-ui/icons/Settings'
-import '../styles/UsersList.css'
-import CategorieUser from './CategorieUser'
-import { UserList } from '../datas/userList'
+import React from "react";
+import SettingsIcon from "@material-ui/icons/Settings";
+import "../styles/UsersList.css";
+import CategorieUser from "./CategorieUser";
+import { UserList } from "../datas/userList";
 
 export default function UsersList() {
-    return (
-        <div className="user__list">
-            <div className="user__list__top">
-                <h2># Général</h2>
-            </div>
-                <div className="user__list__top__icons">
-                    <SettingsIcon />
-                </div>
-            <CategorieUser 
-                UserList={UserList}
-            />
-        </div>   
-    )
+  return (
+    <div className="user__list">
+      <div className="user__list__top">
+        <h2># Général</h2>
+      </div>
+      <div className="user__list__top__icons">
+        <SettingsIcon />
+      </div>
+      <CategorieUser UserList={UserList} />
+    </div>
+  );
 }
-
