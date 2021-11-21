ca3ac4c669c6272738de5f731e887a42595d98b9
FG <francois.gallois.pro@gmail.com>
2021-11-21 20:30:57 +0100

    Checks triviaux des informations de post (A TESTER) Enregistrement intermédiaire, pas encore testé. Vérification des données envoyées par l'utilisateur avec regex puis passage par htmlspecialchars.

--- src/php/signin.php
+++ src/php/signin.php
@@ -1,4 +1,37 @@
 <?php
+// Vérifications de routine, avec renvoi d'erreur par $_SESSION["signuperror"] : vérification de présence, de regex,
+// suppression des specialchars.
+session_start();
+if (!(isset($_POST["mail"]) && (isset($_POST["pseudo"]) && (isset($_POST["password"]) && (isset($_POST["passwordcheck"]))){
+    $_SESSION["signuperror"]="badpost";
+    header('Location : /Project-Plateforme/public/signup.html',true,301);
+    exit();
+}
+if (!filter_var($_POST["mail"],FILTER_VALIDATE_EMAIL)){
+    $_SESSION["signuperror"]="badmail";
+    header('Location : /Project-Plateforme/public/signup.html',true,301);
+    exit();
+}
+if (!preg_match("#^([a-zA-Z0-9._\/\\-]){8,24}$#",$_POST["pseudo"])){
+    $_SESSION["signuperror"]="badpseudo";
+    header('Location : /Project-Plateforme/public/signup.html',true,301);
+    exit();
+}
+if (!preg_match("#^([a-zA-Z0-9._\/\\-]){8,24}$#",$_POST["password"])){
+    $_SESSION["signuperror"]="badpassword";
+    header('Location : /Project-Plateforme/public/signup.html',true,301);
+    exit();
+}
+if (!($_POST["password"]==$_POST["passwordcheck"])){
+    $_SESSION["signuperror"]="unequalpassword";
+    header('Location : /Project-Plateforme/public/signup.html',true,301);
+    exit();
+}
+htmlspecialchars($_POST["mail"]);
+htmlspecialchars($_POST["pseudo"]);
+htmlspecialchars($_POST["password"]);
+htmlspecialchars($_POST["passwordcheck"]);
+
 try
 {
     $db=new PDO('mysql:host=localhost;dbname=logins','root','');
