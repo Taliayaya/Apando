f96d31fd65af64136bdf3cf679312cb625455031
rigobert9 <francois.gallois.pro@gmail.com>
2022-02-04 22:44:13 +0100

    Ajout des clés au gitignore Pour éviter les problèmes

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -14,7 +14,6 @@ import { getFirestore } from 'firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    
 
     authDomain: 'pando-5ec96.firebaseapp.com',
 
