1ed504c2c9c40a584eaa0707b98f6de829c5fc92
Taliayaya <ilanvinord@gmail.com>
2022-03-10 20:27:40 +0100

    Moved File and added Server settings widget

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,13 +2,13 @@ import {
     DashboardBackground,
     DashboardMain,
     DashboardTitle,
-    NumberCase,
     ServerStatsContainer,
 } from './DashboardStyle'
-import PeopleIcon from '@mui/icons-material/People'
+
 import { styled, Tooltip, tooltipClasses } from '@mui/material'
-import GroupAddIcon from '@mui/icons-material/GroupAdd'
-import MessageIcon from '@mui/icons-material/Message'
+
+import { InviteCase, MemberCase, MessageCase } from './StatsCase'
+import ServerParams from './ServerSettings'
 
 const BootstrapTooltip = styled(({ className, ...props }) => (
     <Tooltip {...props} arrow classes={{ popper: className }} />
@@ -21,37 +21,6 @@ const BootstrapTooltip = styled(({ className, ...props }) => (
     },
 }))
 
-const MemberCase = ({ nb }) => {
-    return (
-        <>
-            <NumberCase>
-                {nb} <PeopleIcon sx={{ fontSize: 40 }} />
-            </NumberCase>
-        </>
-    )
-}
-
-const InviteCase = ({ nb }) => {
-    return (
-        <>
-            <NumberCase>
-                {nb} <GroupAddIcon sx={{ fontSize: 40 }} />
-            </NumberCase>
-        </>
-    )
-}
-
-const MessageCase = ({ nb }) => {
-    return (
-        <>
-            <NumberCase>
-                {nb}
-                <MessageIcon sx={{ fontSize: 40 }} />
-            </NumberCase>
-        </>
-    )
-}
-
 const Dashboard = () => {
     const serverName = 'Beta'
     return (
@@ -63,6 +32,11 @@ const Dashboard = () => {
                     <InviteCase nb={3} />
                     <MessageCase nb={500} />
                 </ServerStatsContainer>
+                <ServerParams
+                    serverName={serverName}
+                    code={'EE4SSCËZ34'}
+                    autoJoin="manual"
+                />
             </DashboardMain>
         </DashboardBackground>
     )
