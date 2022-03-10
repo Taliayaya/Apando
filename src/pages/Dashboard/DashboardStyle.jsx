f5be9198d834fec5295b6ea6761a22181eb7fabd
Taliayaya <ilanvinord@gmail.com>
2022-03-10 20:27:18 +0100

    More styled added for widgets and transitions

--- src/pages/Dashboard/DashboardStyle.jsx
+++ src/pages/Dashboard/DashboardStyle.jsx
@@ -34,6 +34,10 @@ const ServerStatsContainer = styled.div`
     border-radius: 30px;
     padding: 15px;
     justify-content: space-around;
+    transition: all 0.3s ease;
+    &:hover {
+        box-shadow: 0 0 15px ${theme.menus_bg_color};
+    }
 `
 
 const NumberCase = styled.div`
@@ -50,16 +54,83 @@ const NumberCase = styled.div`
     line-height: 40px;
     margin: 0 15px;
 
+    transition: all 0.3s;
+
     @media screen and (max-width: 480px) {
         width: 100px;
+    }
+
+    &:hover {
+        box-shadow: 0 0 15px ${theme.chat_input_bg_color};
+        background-color: ${theme.chat_input_bg_color};
     }
 `
 
 const ParamsCase = styled.div`
     background-color: ${theme.menus_bg_color};
+    width: 40%;
+    margin: 40px 0;
+    border-radius: 15px;
+    padding: 20px;
+    color: ${theme.font_color};
+    line-height: 40px;
+    @media screen and (max-width: 480px) {
+        width: 100%;
+    }
+    transition: all 0.3s ease;
+    &:hover {
+        box-shadow: 0 0 15px ${theme.sides_bg_color};
+    }
+`
+const Params = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    color: #aaa;
+    line-height: 30px;
+    background-color: ${theme.sides_bg_color};
+    padding: 20px;
+    margin: 10px 0;
+    border-radius: 30px;
+    transition: all 0.3s ease;
+    &:hover {
+        color: ${theme.font_color};
+        background-color: ${theme.chat_bg_color};
+    }
+`
+
+const ParamsHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+const StyleEditContainer = styled.div`
+    background-color: ${theme.chat_bg_color};
+    border-radius: 60px;
+    cursor: pointer;
+    padding: 4px;
+    align-items: center;
+    justify-content: center;
+    color: ${theme.font_color};
+
+    transition: all 0.3s ease;
+    &:hover {
+        ${(props) =>
+            props.isediting === 'true'
+                ? `background-color: 	#FF6666;`
+                : `background-color: ${theme.chat_input_bg_color};`}
+    }
+    &:focus {
+    }
+`
+const StyledOption = styled.option`
+    color: ${theme.font_color};
+    background-color: ${theme.chat_input_bg_color};
 `
 
 const MemberListCase = styled.div``
+
 export {
     DashboardBackground,
     DashboardTitle,
@@ -67,4 +138,8 @@ export {
     ServerStatsContainer,
     NumberCase,
     ParamsCase,
+    Params,
+    ParamsHeader,
+    StyleEditContainer,
+    StyledOption,
 }
