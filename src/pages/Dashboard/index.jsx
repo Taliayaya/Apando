32689c0ddf02183c1587d1da820e89373b7d686e
Taliayaya <ilanvinord@gmail.com>
2022-03-12 14:39:56 +0100

    Retrieve server id and send it to it's childs. Added member list

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,27 +2,36 @@ import {
     DashboardBackground,
     DashboardMain,
     DashboardTitle,
+    Row2,
     ServerStatsContainer,
 } from './DashboardStyle'
 
-import { styled, Tooltip, tooltipClasses } from '@mui/material'
+// import { styled, Tooltip, tooltipClasses } from '@mui/material'
 
 import { InviteCase, MemberCase, MessageCase } from './StatsCase'
 import ServerParams from './ServerSettings'
+import MemberList from './MemberList'
+import { useChannel } from '../../utils/hooks'
+import { useParams } from 'react-router-dom'
 
-const BootstrapTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} arrow classes={{ popper: className }} />
-))(({ theme }) => ({
-    [`& .${tooltipClasses.arrow}`]: {
-        color: theme.palette.common.black,
-    },
-    [`& .${tooltipClasses.tooltip}`]: {
-        backgroundColor: theme.palette.common.black,
-    },
-}))
+// const BootstrapTooltip = styled(({ className, ...props }) => (
+//     <Tooltip {...props} arrow classes={{ popper: className }} />
+// ))(({ theme }) => ({
+//     [`& .${tooltipClasses.arrow}`]: {
+//         color: theme.palette.common.black,
+//     },
+//     [`& .${tooltipClasses.tooltip}`]: {
+//         backgroundColor: theme.palette.common.black,
+//     },
+// }))
 
 const Dashboard = () => {
+    const { currentServer } = useChannel()
+    console.log(currentServer)
     const serverName = 'Beta'
+    const params = useParams()
+
+    const server_id = currentServer ? currentServer : params.serverid
     return (
         <DashboardBackground>
             <DashboardMain>
@@ -32,11 +41,16 @@ const Dashboard = () => {
                     <InviteCase nb={3} />
                     <MessageCase nb={500} />
                 </ServerStatsContainer>
-                <ServerParams
-                    serverName={serverName}
-                    code={'EE4SSCËZ34'}
-                    autoJoin="manual"
-                />
+                <Row2>
+                    <ServerParams
+                        serverName={serverName}
+                        code={'EE4SSCËZ34'}
+                        autoJoin="manual"
+                        domain={''}
+                        server_id={server_id}
+                    />
+                    <MemberList serverName={serverName} />
+                </Row2>
             </DashboardMain>
         </DashboardBackground>
     )
