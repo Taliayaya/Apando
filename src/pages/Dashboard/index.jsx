4b5281822442b7980f69883520b116ec0158820f
Taliayaya <ilanvinord@gmail.com>
2022-03-13 10:00:07 +0100

    Show the stats and send props : now OP

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -12,7 +12,11 @@ import { InviteCase, MemberCase, MessageCase } from './StatsCase'
 import ServerParams from './ServerSettings'
 import MemberList from './MemberList'
 import { useChannel } from '../../utils/hooks'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getAuth } from 'firebase/auth'
+import { getServerInfo, isUserInTargetServer } from '../../utils/function'
+import { useEffect, useState } from 'react'
+import { getDatabase, onValue, ref } from 'firebase/database'
 
 // const BootstrapTooltip = styled(({ className, ...props }) => (
 //     <Tooltip {...props} arrow classes={{ popper: className }} />
@@ -27,29 +31,70 @@ import { useParams } from 'react-router-dom'
 
 const Dashboard = () => {
     const { currentServer } = useChannel()
-    console.log(currentServer)
-    const serverName = 'Beta'
     const params = useParams()
+    let server_id
+    const user = getAuth().currentUser
+    const navigate = useNavigate()
+    const [serverInfo, setServerInfo] = useState({})
+    const [serverStats, setServerStats] = useState({})
 
-    const server_id = currentServer ? currentServer : params.serverid
+    const checkUser = async () => {
+        const isUserValid = await isUserInTargetServer(user.uid, server_id)
+        if (!isUserValid) {
+            navigate('/app')
+        }
+    }
+    if (currentServer) {
+        server_id = currentServer
+    } else {
+        server_id = params.serverid
+        checkUser()
+    }
+
+    useEffect(() => {
+        const getServer = async () => {
+            const serverInfo = await getServerInfo(server_id)
+            setServerInfo(serverInfo)
+            console.log(serverInfo)
+        }
+
+        getServer()
+    }, [server_id])
+
+    useEffect(() => {
+        const db = getDatabase()
+        const serverStatsRef = ref(db, `/serverstats/${server_id}`)
+        const unsub = onValue(serverStatsRef, (snapshot) => {
+            setServerStats(snapshot.val())
+        })
+
+        return () => unsub()
+    }, [server_id])
+
     return (
         <DashboardBackground>
             <DashboardMain>
-                <DashboardTitle>Dashboard de {serverName}</DashboardTitle>
+                <DashboardTitle>Dashboard de {serverInfo?.name}</DashboardTitle>
                 <ServerStatsContainer>
-                    <MemberCase nb={100} />
-                    <InviteCase nb={3} />
-                    <MessageCase nb={500} />
+                    <MemberCase nb={serverStats?.memberCount} />
+                    <InviteCase nb={serverStats?.currentInviteCount} />
+                    <MessageCase nb={serverStats?.messageCount} />
                 </ServerStatsContainer>
                 <Row2>
                     <ServerParams
-                        serverName={serverName}
-                        code={'EE4SSCËZ34'}
-                        autoJoin="manual"
-                        domain={''}
+                        serverName={serverInfo?.name}
+                        code={serverInfo?.code ? serverInfo.code : ''}
+                        autoJoin={
+                            serverInfo?.jointype ? serverInfo?.jointype : 'auto'
+                        }
+                        domain={serverInfo?.domain ? serverInfo?.domain : ''}
                         server_id={server_id}
                     />
-                    <MemberList serverName={serverName} />
+                    <MemberList
+                        serverName={serverInfo?.name}
+                        server_id={server_id}
+                        server={serverInfo}
+                    />
                 </Row2>
             </DashboardMain>
         </DashboardBackground>
