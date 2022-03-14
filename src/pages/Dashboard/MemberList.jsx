4b81daeeee6e476f3db5d85e6aec1da97e4fff54
Taliayaya <ilanvinord@gmail.com>
2022-03-14 22:09:19 +0100

    Added ban feature from dashboard

--- src/pages/Dashboard/MemberList.jsx
+++ src/pages/Dashboard/MemberList.jsx
@@ -1,8 +1,18 @@
+import { Block, MoreHoriz } from '@material-ui/icons'
 import { DoneAll } from '@mui/icons-material'
-import { Avatar, IconButton, Tooltip } from '@mui/material'
+import {
+    Avatar,
+    IconButton,
+    ListItemIcon,
+    Menu,
+    MenuItem,
+    Tooltip,
+    Typography,
+} from '@mui/material'
 import { getDatabase, onValue, ref } from 'firebase/database'
 import { useEffect, useState } from 'react'
 import {
+    banUserFromServer,
     getServerUserList,
     joinServer,
     removeJoinRequest,
@@ -106,11 +116,12 @@ const MemberList = ({ serverName, server_id, server, joinType }) => {
             <MemberListContainer>
                 {usersArray.map(({ id, data }) => (
                     <UserCase
-                        id={data.id}
+                        id={id}
                         name={data.name}
                         email={data.email}
                         key={id}
                         avatar={data.avatar}
+                        server={serverInfo}
                     />
                 ))}
             </MemberListContainer>
@@ -129,10 +140,23 @@ const UserCase = ({ avatar, name, email, invite, server, id }) => {
     //             : null
     //     )
     // }
+    const [anchorEl, setAnchorEl] = useState(null)
+    const open = Boolean(anchorEl)
+
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
 
     const user = {
         uid: id,
         email: email,
+    }
+    const handleBan = () => {
+        console.log(server, user)
+        banUserFromServer(server.id, user.uid)
     }
     const acceptJoinRequest = () => {
         console.log(user, server)
@@ -150,12 +174,65 @@ const UserCase = ({ avatar, name, email, invite, server, id }) => {
                     <UserEmailStyle>{email}</UserEmailStyle>
                 </UserInfo>
             </UserContainer>
-            {invite === 'true' && (
+            {invite === 'true' ? (
                 <StyleDone
                     sx={{ fontSize: '30px' }}
                     onClick={acceptJoinRequest}
                 />
+            ) : (
+                <Tooltip title="Plus d'actions">
+                    <IconButton
+                        onClick={(e) => handleClick(e)}
+                        aria-controls={open ? 'user-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <MoreHoriz />
+                    </IconButton>
+                </Tooltip>
             )}
+            <Menu
+                anchorEl={anchorEl}
+                id="user-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={handleBan}>
+                    <ListItemIcon>
+                        <Block />
+                    </ListItemIcon>
+                    <Typography>Retirer du serveur</Typography>
+                </MenuItem>
+            </Menu>
         </StyledDiv>
     )
 }
