6575733bf1185666b223527e8e3cd1f2c1274cce
Taliayaya <ilanvinord@gmail.com>
2022-03-15 17:56:04 +0100

    Added Tooltips on Menu buttons

--- src/components/TopMenu/index.jsx
+++ src/components/TopMenu/index.jsx
@@ -4,12 +4,13 @@ import styled from 'styled-components'
 import { useAuth } from '../../utils/hooks'
 import { theme } from '../../utils/style/colors'
 import MenuIcon from '@mui/icons-material/Menu'
+import { IconButton, Tooltip } from '@mui/material'
 
 const StyledMenu = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    padding: 10px;
+    padding: 5px;
     background-color: ${theme.top_menu_bg_color};
 `
 
@@ -17,14 +18,32 @@ const TopMenu = () => {
     const { setShowUsers, showUsers, setShowChannel, showChannel } = useAuth()
     return (
         <StyledMenu>
-            <MenuIcon
-                style={{ cursor: 'pointer', color: theme.font_color }}
-                onClick={() => setShowChannel(!showChannel)}
-            />
-            <People
-                style={{ cursor: 'pointer', color: theme.font_color }}
-                onClick={() => setShowUsers(!showUsers)}
-            />
+            <Tooltip
+                title={
+                    showChannel
+                        ? 'Masquer le menu gauche'
+                        : 'Afficher le menu gauche'
+                }
+            >
+                <IconButton onClick={() => setShowChannel(!showChannel)}>
+                    <MenuIcon
+                        style={{ cursor: 'pointer', color: theme.font_color }}
+                    />
+                </IconButton>
+            </Tooltip>
+            <Tooltip
+                title={
+                    showUsers
+                        ? 'Masquer le menu droit'
+                        : 'Afficher le menu droit'
+                }
+            >
+                <IconButton onClick={() => setShowUsers(!showUsers)}>
+                    <People
+                        style={{ cursor: 'pointer', color: theme.font_color }}
+                    />
+                </IconButton>
+            </Tooltip>
         </StyledMenu>
     )
 }
