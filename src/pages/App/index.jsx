3ad6f1ed3392eb1869696eb19a6cad210d909e79
Taliayaya <ilanvinord@gmail.com>
2022-04-23 23:59:57 +0200

    User verifications before accessing a server

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -2,10 +2,12 @@ import { getAuth } from 'firebase/auth'
 import { doc, Timestamp, updateDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import Helmet from 'react-helmet'
+import { useNavigate, useParams } from 'react-router-dom'
 import ChannelList from '../../components/ChannelList'
 import Chat from '../../components/Chat'
 import UserList from '../../components/UserList'
 import { db } from '../../utils/firebase/config'
+import { getServerInfo, isUserInTargetServer } from '../../utils/function'
 import { useAuth, useChannel } from '../../utils/hooks'
 import { StyledStructure } from './AppStyle'
 
@@ -14,7 +16,53 @@ function App() {
     const user = auth.currentUser
     const { setShowChannel, setShowUsers } = useAuth()
     const [firstMobileTime, setFirstMobileTime] = useState(true)
-    const { currentServer } = useChannel()
+    const { currentServer, setCurrentServer } = useChannel()
+    const params = useParams()
+    const navigate = useNavigate()
+
+    /**
+     * This useEffect is used to check users accessing a server by URL
+     * It redirects if it's accessing an unauthorized server.
+     * Otherwise it updates states according to this server
+     */
+    useEffect(() => {
+        const checkUser = async (server_id) => {
+            console.log(server_id)
+            const isUserValid = await isUserInTargetServer(user.uid, server_id)
+
+            if (isUserValid) {
+                if (currentServer) return
+                const serverData = await getServerInfo(server_id)
+                if (serverData) {
+                    console.log(1)
+                    setCurrentServer({
+                        id: serverData.id,
+                        name: serverData.name,
+                    })
+                    /** Well, the user is in the server but......
+                     * We couldn't retrieve any data ??
+                     * Let's avoid any error, shall we ? Get redirected !
+                     */
+                    return
+                }
+                return
+            }
+            navigate('/app')
+        }
+
+        if (currentServer?.id || params.server_id) {
+            checkUser(currentServer?.id ? currentServer.id : params.server_id)
+        }
+        console.log(1)
+        // Else this user isn't in a server, so no verification needed
+    }, [
+        currentServer,
+        currentServer.id,
+        navigate,
+        params.server_id,
+        setCurrentServer,
+        user.uid,
+    ])
 
     useEffect(() => {
         const setOnline = setInterval(async () => {
