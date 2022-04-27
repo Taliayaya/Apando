d05d08878c8c447df937ff46b907a80026850d2a
Taliayaya <ilanvinord@gmail.com>
2022-04-27 20:15:37 +0200

    repaired requireAuth

--- src/components/RequireAuth/index.jsx
+++ src/components/RequireAuth/index.jsx
@@ -6,7 +6,13 @@ function RequireAuth({ children }) {
     const location = useLocation()
     const navigate = useNavigate()
 
-    !authed && navigate('/login')
+    if (!authed) {
+        // Redirect them to the /login page, but save the current location they were
+        // trying to go to when they were redirected. This allows us to send them
+        // along to that page after they login, which is a nicer user experience
+        // than dropping them off on the home page.
+        return <Navigate to="/login" state={{ from: location }} replace />
+    }
 
     return children
 }
