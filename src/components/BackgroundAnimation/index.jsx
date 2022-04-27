3e3f796e5ecab7808af4ee917fd31239af0a31e7
Taliayaya <ilanvinord@gmail.com>
2022-04-27 20:15:14 +0200

    added doc

--- src/components/BackgroundAnimation/index.jsx
+++ src/components/BackgroundAnimation/index.jsx
@@ -4,12 +4,19 @@ import {
     HomePageTreeAbove,
     HomePageTreeBelow,
 } from '../../pages/Home/HomeStyle'
-
+import PropTypes from 'prop-types'
 import { Cherryblossom } from '../Cherryblossom'
 
+/**
+ * The background menu/animation of Apando.
+ * This will become a seasonal background.
+ * Currently only the spring theme is set.
+ * Summer, Fall and Winter will be out when the time will come~~
+ */
 const BackgroundAnimation = ({ children, sakura }) => {
     return (
         <HomePageBanner>
+            {/* Later to turn on/off the blossom animation */}
             {sakura && <Cherryblossom amount={60} size={10} />}
             <Clouds>
                 <HomePageTreeBelow>
@@ -18,6 +25,11 @@ const BackgroundAnimation = ({ children, sakura }) => {
             </Clouds>
         </HomePageBanner>
     )
+}
+
+BackgroundAnimation.propTypes = {
+    children: PropTypes.node,
+    sakura: PropTypes.bool,
 }
 
 export default BackgroundAnimation
