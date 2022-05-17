d26f3975118059ab10d65765c736abea832d4b5d
Taliayaya <ilanvinord@gmail.com>
2022-05-17 22:44:39 +0200

    MY ENNEMY, huge bug, the F5 no CSS one

--- src/utils/style/GlobalStyle.jsx
+++ src/utils/style/GlobalStyle.jsx
@@ -1,16 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const StyledGlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
-@font-face {
-    font-family: "Gravity";
-    src: url("./fonts/Gravity-Italic.otf");
-  }
-@font-face {
-    font-family: "Tuffy";
-    src: url("fonts/Tuffy-Bold.ttf");
-  }
-
 body {
     margin: 0 !important;
     padding: 0 !important;
