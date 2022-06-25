09f1fd1fdcff416c23f7a7b628913809ff5ca9fe
Taliayaya <ilanvinord@gmail.com>
2022-06-25 22:21:43 +0200

    cookie handle functions

--- src/utils/function.js
+++ src/utils/function.js
@@ -473,6 +473,28 @@ function removeJoinRequest(uid, id_server) {
     updateInviteCount(id_server, -1)
 }
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date()
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+    let expires = 'expires=' + d.toUTCString()
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+}
+
+function getCookie(cname) {
+    let name = cname + '='
+    let decodedCookie = decodeURIComponent(document.cookie)
+    let ca = decodedCookie.split(';')
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
+        }
+    }
+    return ''
+}
 export {
     writeUserMessage,
     writeUserRole,
@@ -493,4 +515,6 @@ export {
     requestJoin,
     removeJoinRequest,
     setMessageAsSeen,
+    getCookie,
+    setCookie,
 }
