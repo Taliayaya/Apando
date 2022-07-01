f5e4e54d0589a080f9d08f85695a6c4289f6ca63
Taliayaya <ilanvinord@gmail.com>
2022-07-01 20:57:32 +0200

    Adding collections & server system

--- src/pages/Create/Organisation/Steps/Step2/index.jsx
+++ src/pages/Create/Organisation/Steps/Step2/index.jsx
@@ -1,7 +1,10 @@
 import { NavigateNext } from '@mui/icons-material'
-import { Breadcrumbs, Link, Typography } from '@mui/material'
+import { Breadcrumbs, Typography } from '@mui/material'
 import React from 'react'
+import AddCollectionDialog from './AddCollectionDialog'
+import AddServerDialog from './AddServerDialog'
 import Collection from './Collection'
+import ServerBox from './ServerBox'
 import {
     AddCollectionDiv,
     AddRoundedStyle,
@@ -14,11 +17,13 @@ const subCollectionTemplate = [
         name: 'Terminal',
         description: 'Les bests',
         subCollection: [],
+        servers: [],
     },
     {
         name: 'Première',
         description: 'moi avant',
         subCollection: [],
+        servers: [],
     },
 ]
 const collectionsTest = [
@@ -26,18 +31,37 @@ const collectionsTest = [
         name: 'Lycée',
         description: '',
         subCollection: subCollectionTemplate,
+        servers: [],
     },
-    { name: 'Collège', description: 'Pour le collège', subCollection: [] },
-    { name: 'Primaire', description: 'Pour le primaire', subCollection: [] },
+    {
+        name: 'Collège',
+        description: 'Pour le collège',
+        subCollection: [],
+        servers: [],
+    },
+    {
+        name: 'Primaire',
+        description: 'Pour le primaire',
+        subCollection: [],
+        servers: [],
+    },
 ]
 
 function Step2({ orgaInfo, setOrgaInfo }) {
     const [collections, setCollections] = React.useState(collectionsTest)
     const [depth, setDepth] = React.useState([])
+    const [open, setOpen] = React.useState(false)
+    const [openServerDialog, setOpenServerDialog] = React.useState(false)
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleCloseServerDialog = () => setOpenServerDialog(false)
 
     const subCollections =
         depth.length > 0 &&
-        collections.filter((value) => value.name === depth[0])[0].subCollection
+        collections.filter((value) => value.name === depth[0])[0]
 
     const servers =
         depth.length === 2 &&
@@ -49,10 +73,10 @@ function Step2({ orgaInfo, setOrgaInfo }) {
             display = collections
             break
         case 1:
-            display = subCollections
+            display = subCollections.subCollection
             break
         case 2:
-            display = subCollections
+            display = subCollections.subCollection
             break
         default:
             display = collections
@@ -65,6 +89,30 @@ function Step2({ orgaInfo, setOrgaInfo }) {
         console.log(newDepth)
         setDepth(newDepth)
     }
+
+    const handleDeleteCollection = (name) => {
+        const newArray = collections.filter((values) => name !== values.name)
+        setCollections(newArray)
+    }
+
+    const handleAddSubCollection = (newCollection, parent) => {
+        collections
+            .find((element) => element.name === parent)
+            .subCollection.push(newCollection)
+
+        console.log('collections', collections)
+        setCollections(collections)
+    }
+
+    const handleAddServer = (serverInfo) => {
+        collections
+            .find((element) => element.name === depth[0])
+            .servers.push(serverInfo)
+
+        console.log(collections)
+        setCollections(collections)
+    }
+
     return (
         <React.Fragment>
             {/* <h2>Ajouter des serveurs</h2> */}
@@ -100,17 +148,47 @@ function Step2({ orgaInfo, setOrgaInfo }) {
                         subCollection={subCollection}
                         depth={depth}
                         setDepth={setDepth}
+                        handleDelete={handleDeleteCollection}
+                        handleAddSubCollection={handleAddSubCollection}
                     />
                 ))}
-                <AddCollectionDiv>
-                    <AddRoundedStyle sx={{ fontSize: 100 }} />
-                    <Typography
-                        style={{ marginTop: '10px' }}
-                        color="text.secondary"
-                    >
-                        Ajouter une collection
-                    </Typography>
-                </AddCollectionDiv>
+                {depth.length > 0 &&
+                    subCollections.servers.map((server) => (
+                        <ServerBox {...server} />
+                    ))}
+                {depth.length < 2 && (
+                    <AddCollectionDiv onClick={() => setOpen(true)}>
+                        <AddRoundedStyle sx={{ fontSize: 100 }} />
+                        <Typography
+                            style={{ marginTop: '10px' }}
+                            color="text.secondary"
+                        >
+                            Ajouter une collection
+                        </Typography>
+                    </AddCollectionDiv>
+                )}
+                {depth.length > 0 && (
+                    <AddCollectionDiv onClick={() => setOpenServerDialog(true)}>
+                        <AddRoundedStyle sx={{ fontSize: 100 }} />
+                        <Typography
+                            style={{ marginTop: '10px' }}
+                            color="text.secondary"
+                        >
+                            Ajouter un serveur
+                        </Typography>
+                    </AddCollectionDiv>
+                )}
+                <AddCollectionDialog
+                    open={open}
+                    onClose={handleClose}
+                    collections={collections}
+                    setCollections={setCollections}
+                />
+                <AddServerDialog
+                    open={openServerDialog}
+                    onClose={handleCloseServerDialog}
+                    handleAddServer={handleAddServer}
+                />
             </CollectionsContainer>
         </React.Fragment>
     )
