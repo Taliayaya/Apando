ee1fadc6ff2b926d256b669330ce49d3d4c75fe5
Taliayaya <ilanvinord@gmail.com>
2022-07-06 20:41:31 +0200

    Added Step3

--- src/pages/Create/Organisation/Steps/index.jsx
+++ src/pages/Create/Organisation/Steps/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Step1 from './Step1'
 import Step2 from './Step2'
+import Step3 from './Step3'
 
 function Steps({ activeStep, orgaInfo, setOrgaInfo }) {
     switch (activeStep) {
@@ -8,6 +9,8 @@ function Steps({ activeStep, orgaInfo, setOrgaInfo }) {
             return <Step1 orgaInfo={orgaInfo} setOrgaInfo={setOrgaInfo} />
         case 1:
             return <Step2 orgaInfo={orgaInfo} setOrgaInfo={setOrgaInfo} />
+        case 2:
+            return <Step3 orgaInfo={orgaInfo} setOrgaInfo={setOrgaInfo} />
         default:
             return null
     }
